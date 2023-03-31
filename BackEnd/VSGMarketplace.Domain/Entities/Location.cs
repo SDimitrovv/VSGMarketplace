af82309be89434af8180ab8816483f95ca16dc52
Stoyan Dimitrov <SDimitrov@VSG.COM>
2023-03-31 15:05:14 +0300

    Added location entity

---
+++ BackEnd/VSGMarketplace.Domain/Entities/Location.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace VSGMarketplace.Domain.Entities
+{
+    [Table("Locations")]
+    public class Location : BaseEntity
+    {
+        public string City { get; set; }
+
+        public int ItemId { get; set; }
+    }
+}
