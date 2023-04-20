7c41a7114403449a20e78077993e48899cc1b8e2
Stoyan Dimitrov <SDimitrov@vsgbg.com>
2023-04-20 15:19:15 +0300

    Transaction bug fixed

--- BackEnd/MarketplaceInfrastructure/Repositories/GenericRepository.cs
+++ BackEnd/MarketplaceInfrastructure/Repositories/GenericRepository.cs
@@ -20,33 +20,33 @@ namespace MarketplaceInfrastructure.Repositories
 
         public async Task<IEnumerable<T>> GetAll()
         {
-            var collection = await Connection.GetListAsync<T>();
+            var collection = await Connection.GetListAsync<T>(null, null, Transaction);
 
             return collection;
         }
 
         public async Task<T> GetByID(int id)
         {
-            var entity = await Connection.GetAsync<T>(id);
+            var entity = await Connection.GetAsync<T>(id, Transaction);
 
             return entity;
         }
 
         public async Task<int> Create(T entity)
         {
-            var recordId = (int)await Connection.InsertAsync<T>(entity);
+            var recordId = (int)await Connection.InsertAsync<T>(entity, Transaction);
 
             return recordId;
         }
 
         public async Task Update(T entity)
         {
-            await Connection.UpdateAsync<T>(entity);
+            await Connection.UpdateAsync<T>(entity, Transaction);
         }
 
         public async Task Delete(int id)
         {
-            await Connection.DeleteAsync<T>(id);
+            await Connection.DeleteAsync<T>(id, Transaction);
         }
     }
 }
