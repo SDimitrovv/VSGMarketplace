c1bcbf11fcabaec1ed18bf00c926cd5a4d19c076
Stoyan Dimitrov <SDimitrov@vsgbg.com>
2023-04-21 16:44:39 +0300

    Validation + ex handling middleware done

--- BackEnd/MarketplaceApplication/Helpers/Validators/AddOrderValidator.cs
+++ BackEnd/MarketplaceApplication/Helpers/Validators/AddOrderValidator.cs
@@ -1,12 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using FluentValidation;
+using MarketplaceApplication.Models.OrderModels.DTOs;
 
 namespace MarketplaceApplication.Helpers.Validators
 {
-    public class AddOrderValidator
+    public class AddOrderValidator : AbstractValidator<AddOrderModel>
     {
+        public AddOrderValidator()
+        {
+            RuleFor(c => c.Quantity)
+                .NotNull().WithMessage("Quantity can't be null")
+                .GreaterThan(-1).WithMessage("Quantity must be positive number!");
+
+            RuleFor(c => c.ProductId)
+                .NotNull().WithMessage("ProductId can't be null")
+                .GreaterThan(-1).WithMessage("ProductId must be positive number!");
+
+            RuleFor(c => c.Email)
+                .NotNull().WithMessage("Email can't be null!")
+                .Length(3, 255);
+        }
     }
 }
