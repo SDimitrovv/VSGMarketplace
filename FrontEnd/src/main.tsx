ed7750b918b1a55ec411f6abc29ba380c687a9de
Ediz Redzhepov <eredzhepov@vsgbg.com>
2023-06-15 16:27:53 +0300

    Changed import of styles.scss location

--- FrontEnd/src/main.tsx
+++ FrontEnd/src/main.tsx
@@ -1,9 +1,10 @@
 import ReactDOM from 'react-dom/client';
 // import React from 'react';
+import '/src/styles.scss';
 import App from './App.tsx';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   // <React.StrictMode>
-    <App />
+  <App />
   // </React.StrictMode>,
 )
